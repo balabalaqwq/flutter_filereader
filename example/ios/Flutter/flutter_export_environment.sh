6a20d8c36424c3f77854bfce89eed92c5e83154d
Ant <weibintao@orange1.com.cn>
2022-10-19 15:29:06 +0800

    修复iOS报错bug
    修复iOS报错bug

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/qipengxiang/fvm/versions/3.0.5"
-export "FLUTTER_APPLICATION_PATH=/Users/qipengxiang/project/Flutter/leadigital/file_reader/example"
+export "FLUTTER_ROOT=/Users/mac/fvm/versions/2.8.1"
+export "FLUTTER_APPLICATION_PATH=/Users/mac/Desktop/github/flutter_filereader/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
@@ -10,4 +10,4 @@ export "FLUTTER_BUILD_NUMBER=11"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=false"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=.dart_tool/package_config.json"
+export "PACKAGE_CONFIG=.packages"
